2f3ff635d32e2b2c0ece0fc1ad56b3dd15a1e10e
mega-flygon <snyderc@gmx.com>
2017-12-17 17:17:09 -0800

    now all buttons work and basic functionality is there, but problems with typing new numbers after pressing equals.

--- Calculator/Form1.cs
+++ Calculator/Form1.cs
@@ -12,24 +12,226 @@ namespace Calculator
 {
     public partial class Form1 : Form
     {
+        enum Operation { ADD, SUBTRACT, MULTIPLY, DIVIDE };
+
+        private double currentNumber = 0;
+        private double runningTotal = 0;
+        private Operation currentOperation = Operation.ADD;
+        private bool overwriteTotal = false; // set this to false everytime it's used in logic
+
         public Form1()
         {
             InitializeComponent();
+            display.SelectionAlignment = HorizontalAlignment.Right;
         }
 
-        private void button12_Click(object sender, EventArgs e)
+        private void button0_Click(object sender, EventArgs e)
         {
-
+            if (overwriteTotal) {
+                runningTotal = 0;
+                overwriteTotal = false;
+            }
+            currentNumber *= 10;
+            currentNumber += 0;
+            display.Text = Convert.ToString(currentNumber);
+            display.SelectionAlignment = HorizontalAlignment.Right;
         }
 
-        private void button16_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)
         {
+            if (overwriteTotal)
+            {
+                runningTotal = 0;
+                overwriteTotal = false;
+            }
+            currentNumber *= 10;
+            currentNumber += 1;
+            display.Text = Convert.ToString(currentNumber);
+            display.SelectionAlignment = HorizontalAlignment.Right;
+        }
 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (overwriteTotal)
+            {
+                runningTotal = 0;
+                overwriteTotal = false;
+            }
+            currentNumber *= 10;
+            currentNumber += 2;
+            display.Text = Convert.ToString(currentNumber);
+            display.SelectionAlignment = HorizontalAlignment.Right;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (overwriteTotal)
+            {
+                runningTotal = 0;
+                overwriteTotal = false;
+            }
+            currentNumber *= 10;
+            currentNumber += 3;
+            display.Text = Convert.ToString(currentNumber);
+            display.SelectionAlignment = HorizontalAlignment.Right;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (overwriteTotal)
+            {
+                runningTotal = 0;
+                overwriteTotal = false;
+            }
+            currentNumber *= 10;
+            currentNumber += 4;
+            display.Text = Convert.ToString(currentNumber);
+            display.SelectionAlignment = HorizontalAlignment.Right;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (overwriteTotal)
+            {
+                runningTotal = 0;
+                overwriteTotal = false;
+            }
+            currentNumber *= 10;
+            currentNumber += 5;
+            display.Text = Convert.ToString(currentNumber);
+            display.SelectionAlignment = HorizontalAlignment.Right;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (overwriteTotal)
+            {
+                runningTotal = 0;
+                overwriteTotal = false;
+            }
+            currentNumber *= 10;
+            currentNumber += 6;
+            display.Text = Convert.ToString(currentNumber);
+            display.SelectionAlignment = HorizontalAlignment.Right;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (overwriteTotal)
+            {
+                runningTotal = 0;
+                overwriteTotal = false;
+            }
+            currentNumber *= 10;
+            currentNumber += 7;
+            display.Text = Convert.ToString(currentNumber);
+            display.SelectionAlignment = HorizontalAlignment.Right;
         }
 
         private void button8_Click(object sender, EventArgs e)
         {
-
+            if (overwriteTotal)
+            {
+                runningTotal = 0;
+                overwriteTotal = false;
+            }
+            currentNumber *= 10;
+            currentNumber += 8;
+            display.Text = Convert.ToString(currentNumber);
+            display.SelectionAlignment = HorizontalAlignment.Right;
         }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (overwriteTotal)
+            {
+                runningTotal = 0;
+                overwriteTotal = false;
+            }
+            currentNumber *= 10;
+            currentNumber += 9;
+            display.Text = Convert.ToString(currentNumber);
+            display.SelectionAlignment = HorizontalAlignment.Right;
+        }
+
+        private void equalsButton_Click(object sender, EventArgs e)
+        {
+            performCurrentOperation();
+            overwriteTotal = true;
+            display.Text = Convert.ToString(runningTotal);
+            display.SelectionAlignment = HorizontalAlignment.Right;
+        }
+
+        public void performCurrentOperation()
+        {
+            switch (currentOperation)
+            {
+                case Operation.ADD:
+                    runningTotal += currentNumber;
+                    break;
+                case Operation.SUBTRACT:
+                    runningTotal -= currentNumber;
+                    break;
+                case Operation.MULTIPLY:
+                    runningTotal *= currentNumber;
+                    break;
+                case Operation.DIVIDE:
+                    runningTotal /= currentNumber;
+                    break;
+            };
+            currentNumber = 0;
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            if (overwriteTotal) overwriteTotal = false;
+            performCurrentOperation();
+            display.Text = Convert.ToString(runningTotal);
+            display.SelectionAlignment = HorizontalAlignment.Right;
+            currentOperation = Operation.ADD;
+        }
+
+        private void subtractButton_Click(object sender, EventArgs e)
+        {
+            if (overwriteTotal) overwriteTotal = false;
+            performCurrentOperation();
+            display.Text = Convert.ToString(runningTotal);
+            display.SelectionAlignment = HorizontalAlignment.Right;
+            currentOperation = Operation.SUBTRACT;
+        }
+
+        private void multiplyButton_Click(object sender, EventArgs e)
+        {
+            if (overwriteTotal) overwriteTotal = false;
+            performCurrentOperation();
+            display.Text = Convert.ToString(runningTotal);
+            display.SelectionAlignment = HorizontalAlignment.Right;
+            currentOperation = Operation.MULTIPLY;
+        }
+
+        private void divideButton_Click(object sender, EventArgs e)
+        {
+            if (overwriteTotal) overwriteTotal = false;
+            performCurrentOperation();
+            display.Text = Convert.ToString(runningTotal);
+            display.SelectionAlignment = HorizontalAlignment.Right;
+            currentOperation = Operation.DIVIDE;
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            currentNumber = 0;
+            runningTotal = 0;
+            currentOperation = Operation.ADD;
+            display.Text = Convert.ToString(currentNumber);
+            display.SelectionAlignment = HorizontalAlignment.Right;
+        }
+
+        private void decimalButton_Click(object sender, EventArgs e)
+        {
+            currentNumber = 0;
+            display.Text = "TOO HARD";
+        }
+
     }
 }
